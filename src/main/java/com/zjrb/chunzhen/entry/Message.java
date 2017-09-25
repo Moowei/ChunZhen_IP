d52c9fba29e50315d5e010174bbe356017057f89
Moowei <990624607@qq.com>
2017-09-25 19:58:28 +0800

    add pro

---
+++ src/main/java/com/zjrb/chunzhen/entry/Message.java
@@ -0,0 +1,10 @@
+package com.zjrb.chunzhen.entry;
+
+/**
+ * Created by blw on 2017/9/19.
+ */
+public interface Message {
+    String bad_ip_file="IP地址库文件错误";
+    String unknown_country="未知国家";
+    String unknown_area="未知地区";
+}
---
+++ src/main/java/com/zjrb/chunzhen/util/LogFactory.java
@@ -0,0 +1,29 @@
+package com.zjrb.chunzhen.util;
+
+/**
+ * Created by blw on 2017/9/19.
+ */
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ *
+ * 日志工厂
+ */
+public class LogFactory {
+    private static final Logger logger;
+    static {
+        logger = Logger.getLogger("stdout");
+        logger.setLevel(Level.DEBUG);
+    }
+
+    public static void log(String info, Level level, Throwable ex) {
+        logger.log(level, info, ex);
+    }
+
+    public static Level  getLogLevel(){
+        return logger.getLevel();
+    }
+
+}
